3b35261feed45281a465091fb8cb0f6bd25a9567
Daniel <daniel.barquero.dev@gmail.com>
2026-05-12 15:28:21 -0600

    chore: remove testing (temp)

--- scripts/prisma-test.js
+++
@@ -1,5 +0,0 @@
-const { execSync } = require('child_process');
-require('dotenv').config({ path: '.env.test', override: true });
-
-const args = process.argv.slice(2).join(' ');
-execSync(`npx prisma ${args}`, { stdio: 'inherit', env: process.env });
